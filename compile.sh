a6c7a2fcf75242061d4a02f80456ff57ef87a877
Sebastián E. Peyrott <sebastian.peyrott@auth0.com>
2017-07-05 19:10:53 -0300

    Refer to correct Docker image.

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run -t -i -v $(pwd)/src:/build/jerryscript/targets/particle/source -v $(pwd)/Makefile.particle:/build/jerryscript/targets/particle/Makefile.particle -v $(pwd)/custom.profile:/build/jerryscript/jerry-core/profiles/custom.profile -v $(pwd)/dist:/tmp/dist --rm sebadoom/jerryphoton /bin/bash -c 'cd /build/jerryscript && make -f ./targets/particle/Makefile.particle && cp ./build/particle/jerry_main.bin /tmp/dist/firmware.bin'
+docker run -t -i -v $(pwd)/src:/build/jerryscript/targets/particle/source -v $(pwd)/Makefile.particle:/build/jerryscript/targets/particle/Makefile.particle -v $(pwd)/custom.profile:/build/jerryscript/jerry-core/profiles/custom.profile -v $(pwd)/dist:/tmp/dist --rm sebadoom/jerryphoton:sensor_hub /bin/bash -c 'cd /build/jerryscript && make -f ./targets/particle/Makefile.particle && cp ./build/particle/jerry_main.bin /tmp/dist/firmware.bin'
 
